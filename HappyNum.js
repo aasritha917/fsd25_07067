dce8c948c4151218841407750b1586f2fe3e979e
aasritha917 <146427855+aasritha917@users.noreply.github.com>
2025-02-28 10:46:46 +0530

    Add files via upload

---
+++ HappyNum.js
@@ -0,0 +1,23 @@
+function isHappy(n) {
+    let seen = new Set(); 
+
+    while (n !== 1 && !seen.has(n)) {
+        seen.add(n); 
+        n = sumOfSquares(n); 
+    }
+
+    return n === 1; 
+}
+
+function sumOfSquares(num) {
+    let sum = 0;
+    while (num > 0) {
+        let digit = num % 10; 
+        sum += digit * digit; 
+        num = Math.floor(num / 10); 
+    }
+    return sum;
+}
+
+console.log(isHappy(19)); 
+console.log(isHappy(20)); 
