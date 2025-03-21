7cbc4e578b8436c7a6c07c034565baebf81b2f15
Kaluvala Aasritha <aasrithaaishu@gmail.com>
2025-03-21 19:55:28 +0530

    Promises Assessment

---
+++ 21-03/Promises/DataFetching.js
@@ -0,0 +1,23 @@
+function fetchData() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const success = Math.random() > 0.5
+        if (success) {
+          resolve("Fetched data successfully!")
+        } else {
+          reject("Network error occurred")
+        }
+      }, 1000);
+    });
+  }
+
+  async function fetchDataHandler() {
+    try {
+      const data = await fetchData();
+      console.log(data);
+    } catch (error) {
+      console.log("Error fetching data:", error);
+    }
+  }
+  fetchDataHandler();
+  
